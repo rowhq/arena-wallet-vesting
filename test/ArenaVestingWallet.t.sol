82201f0b96570ee62232439fbf7bc2606f2fb746
Edmundo Arias <Edmpulasky@gmail.com>
2025-09-18 16:38:35 -0500

    fix transfer ownership test

--- test/ArenaVestingWallet.t.sol
+++ test/ArenaVestingWallet.t.sol
@@ -11,25 +11,6 @@ contract ArenaWalletVestingTest is Test {
 
     function setUp() public {}
 
-    function testRecoversArenaOnRenounceOwnership() public {
-        address beneficiary = makeAddr("beneficiary");
-
-        uint64 start = uint64(vm.getBlockTimestamp() + 1 days);
-        uint64 duration = 365 days;
-
-        ArenaVestingWallet app = _create(beneficiary, start, duration);
-
-        address recover = app.recover();
-        uint256 amount = 1_000e18;
-        deal(address(app.ARENA()), address(app), amount);
-
-        vm.prank(beneficiary);
-        app.renounceOwnership();
-
-        assertEq(app.owner(), address(0));
-        assertEq(app.ARENA().balanceOf(recover), amount);
-    }
-
     function testCannotTransferOwnership() public {
         address beneficiary = makeAddr("beneficiary");
         uint64 start = uint64(vm.getBlockTimestamp() + 1 days);
@@ -43,6 +24,13 @@ contract ArenaWalletVestingTest is Test {
     }
 
     function _create(address beneficiary, uint64 start, uint64 duration) internal returns (ArenaVestingWallet) {
-        return new ArenaVestingWallet(beneficiary, start, duration);
+        ArenaVestingWallet.VestingParams memory params = ArenaVestingWallet.VestingParams({
+            beneficiary: beneficiary,
+            cliff: start,
+            start: start,
+            intervals: duration / 30 days,
+            intervalDuration: 30 days
+        });
+        return new ArenaVestingWallet(params);
     }
 }
