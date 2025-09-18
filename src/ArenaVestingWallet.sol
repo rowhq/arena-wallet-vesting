779407ce3b5ef51fb3d92f405bcc59e5aed6d88e
Edmundo Arias <Edmpulasky@gmail.com>
2025-09-18 16:37:26 -0500

    change to vesting intervals and handle arena only

--- src/ArenaVestingWallet.sol
+++ src/ArenaVestingWallet.sol
@@ -2,20 +2,77 @@
 
 pragma solidity 0.8.25;
 
-import "openzeppelin-contracts/contracts/finance/VestingWallet.sol";
+import {VestingWalletCliff, VestingWallet} from "openzeppelin-contracts/contracts/finance/VestingWalletCliff.sol";
+import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract ArenaVestingWallet is VestingWallet {
+contract ArenaVestingWallet is VestingWalletCliff {
     using SafeERC20 for IERC20;
 
     IERC20 public constant ARENA = IERC20(0xB8d7710f7d8349A506b75dD184F05777c82dAd0C);
 
-    address public recover = address(1000);
+    uint256 public amount;
+    bool public started;
+    uint64 public intervals;
+
+    struct VestingParams {
+        address beneficiary;
+        uint64 cliff;
+        uint64 start;
+        uint64 intervals;
+        uint64 intervalDuration;
+    }
+
+    event Arena_VestingDeposit(address indexed token, uint256 amount);
 
     error Arena_CannotTransferOwnership();
+    error Arena_CannotRenounceOwnership();
+    error Arena_InvalidAmount();
+    error Arena_VestingStarted(bool started);
+    error Arena_InvalidParams();
+    error Arena_InvalidToken(address token);
 
-    constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
-        VestingWallet(beneficiaryAddress, startTimestamp, durationSeconds)
-    {}
+    constructor(VestingParams memory params)
+        VestingWalletCliff(params.cliff)
+        VestingWallet(params.beneficiary, params.start, params.intervals * params.intervalDuration)
+    {
+        if (params.beneficiary == address(0) || params.intervals == 0 || params.intervalDuration == 0) {
+            revert Arena_InvalidParams();
+        }
+    }
+
+    /**
+     * @notice Deposits the total amount of ARENA tokens to be vested (single deposit ever).
+     * @dev post transfer of Arena or tokens will not be vested.
+     */
+    function deposit(uint256 _amount) external {
+        if (started) revert Arena_VestingStarted(true);
+        if (_amount == 0) revert Arena_InvalidAmount();
+
+        ARENA.safeTransferFrom(msg.sender, address(this), _amount);
+        amount = _amount;
+        started = true;
+
+        emit Arena_VestingDeposit(address(ARENA), _amount);
+    }
+
+    /**
+     * @dev Release only after vesting has started.
+     */
+    function release(address token) public override {
+        if (!started) revert Arena_VestingStarted(false);
+
+        super.release(token);
+    }
+
+    /**
+     * @notice Calculates the amount of ARENA that has already vested.
+     * @dev other tokens will always return 0 and post ARENA transfers will not be vested.
+     */
+    function vestedAmount(address token, uint64 timestamp) public view override returns (uint256) {
+        if (token != address(ARENA)) revert Arena_InvalidToken(token);
+
+        return _vestingSchedule(amount, timestamp);
+    }
 
     /**
      * @notice Cannot be transferred to another beneficiary.
@@ -27,10 +84,33 @@ contract ArenaVestingWallet is VestingWallet {
     /**
      * @notice Renouncing ownership will transfer all locked ARENA tokens to the vestor address.
      */
-    function renounceOwnership() public override onlyOwner {
-        uint256 lockedAmount = ARENA.balanceOf(address(this));
-        ARENA.safeTransfer(recover, lockedAmount);
+    function renounceOwnership() public view override onlyOwner {
+        revert Arena_CannotRenounceOwnership();
+    }
 
-        super.renounceOwnership();
+    /**
+     * IMPORTANT: The maximum possible loss is always intervals - 1 wei
+     * the amount released at the end of the vesting duration is ~100% (99.99...%).
+     */
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp)
+        internal
+        view
+        override(VestingWalletCliff)
+        returns (uint256)
+    {
+        if (timestamp < cliff()) return 0;
+        if (timestamp >= end()) return totalAllocation;
+        if (released() >= totalAllocation) return totalAllocation;
+
+        uint256 intervalDuration = duration() / intervals;
+
+        uint256 elapsedIntervals = (timestamp - start()) / intervalDuration;
+        if (elapsedIntervals > intervals) elapsedIntervals = intervals;
+
+        uint256 vested = (totalAllocation * elapsedIntervals) / intervals;
+
+        if (vested > totalAllocation) vested = totalAllocation;
+
+        return vested;
     }
 }
